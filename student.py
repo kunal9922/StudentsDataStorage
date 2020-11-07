f18c20df635fde6f303643584bbd8a54a009d594
kunal <53283003+kunal9922@users.noreply.github.com>
2020-11-07 22:03:33 +0530

    Create Main window where all widgets will encapsulate.

--- student.py
+++ student.py
@@ -4,6 +4,16 @@ import mysql.connector
 class StudentRecord:
 	def __init__(self, root):
 		self.root = root
+		title = Label(self.root, text="Student Management System", bd=5,relief="groove",font=("Cambria",32,"bold"),bg="yellow", fg="red")
+		title.pack(side=TOP, fill=X)
+		self.root.title("Student Management System")
+		# take the user monitor width and Height
+		self.screenWidth = self.root.winfo_screenwidth()
+		self.screenHeight = self.root.winfo_screenheight()
 
+		self.root.geometry(f'{self.screenWidth}x{self.screenHeight}+0+0')
 
-root = StudentRecord(Tk())
+if __name__ == "__main__":
+	win = Tk()
+	root = StudentRecord(win)
+	win.mainloop()
