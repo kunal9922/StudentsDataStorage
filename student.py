d67f4002b2b093b189060c2801e87dc12eab76a3
kunal <53283003+kunal9922@users.noreply.github.com>
2020-11-07 23:05:30 +0530

    first insert field added on manageFrame

--- student.py
+++ student.py
@@ -13,13 +13,21 @@ class StudentRecord:
 
 		self.root.geometry(f'{self.screenWidth}x{self.screenHeight}+0+0')
 
-		# This frame contain txt box where  values will insert
-		manageFrame = LabelFrame(self.root, text="Mange Data",font=("Time Roman",20,"bold"), bd=4, fg='Black', bg="#ff9933")
+		# =============== Manage Frame =================
+				# This frame contain txt box where  values will insert
+		manageFrame = LabelFrame(self.root, text="Mange Data", font=("Time Roman",20,"bold"), bd=4, fg='Black', bg="#ff9933")
 		manageFrame.place(x=30, y=70, width=450, height=590)
 
-		# This frame contain list box where  values will shows.
-		manageFrame = LabelFrame(self.root, text="Showing Records", font=("Time Roman", 20, "bold"), bd=4, fg='Black', bg="#ff9933")
-		manageFrame.place(x=500, y=70, width=700, height=590)
+		lbl_roll = Label(manageFrame, text="Roll No : ", font=("", 18, "bold"), fg="#adfc03", bg="#ff9933")
+		lbl_roll.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+
+		txt_roll = Entry(manageFrame, font=("Consolas", 15, "bold"), bd=2, relief="ridge", width=25)
+		txt_roll.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+		# ============== Record Showing Frame ================
+				# This frame contain list box where  values will shows.
+		recordFrame = LabelFrame(self.root, text="Showing Records", font=("Time Roman", 20, "bold"), bd=4, fg='Black', bg="#ff9933")
+		recordFrame.place(x=500, y=70, width=800, height=590)
 
 if __name__ == "__main__":
 	win = Tk()
