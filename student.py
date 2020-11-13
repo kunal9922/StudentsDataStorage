ec016cf2386005932d1a60199552032e98767a72
kunal <53283003+kunal9922@users.noreply.github.com>
2020-11-13 20:38:25 +0530

    Search and show btn added for operation on RecordFrame

--- student.py
+++ student.py
@@ -17,7 +17,7 @@ class StudentRecord:
 		# =============== Manage Frame =================
 				# This frame contain txt box where  values will insert
 		manageFrame = LabelFrame(self.root, text="Manage Data", font=("Time Roman",20,"bold"), bd=4, fg='Black', bg="#ff9933")
-		manageFrame.place(x=30, y=70, width=460, height=660)
+		manageFrame.place(x=30, y=70, width=460, height=600)
 
 		lbl_roll = Label(manageFrame, text="Roll No : ", font=("", 18, "bold"), fg="#adfc03", bg="#ff9933")
 		lbl_roll.grid(row=0, column=0, padx=10, pady=10, sticky="w")
@@ -46,7 +46,7 @@ class StudentRecord:
 
 		lbl_gender = Label(manageFrame, text="Gender : ", font=("Consolas", 18, "bold"), fg="#adfc03", bg="#ff9933")
 		lbl_gender.grid(row=5, column=0, padx=10, pady=10, sticky="w")
-		com_gen = ttk.Combobox(manageFrame, font=("Consolas", 18, "bold"))
+		com_gen = ttk.Combobox(manageFrame, font=("Consolas", 18, "bold"),state="readonly")
 		com_gen["values"] = ("Male", "Female", "Other")
 		com_gen.grid(row=5, column=1, padx=10, pady=10)
 
@@ -59,17 +59,30 @@ class StudentRecord:
 		btnFrame = LabelFrame(manageFrame, bd=6, relief ="solid", fg='Black', bg="#7bfc03")
 		btnFrame.place(x=10, y=500, width=430)
 
-		addbtn = Button(btnFrame, text="Add", width=10).grid(row=0,column=0, padx=10,pady=10)
-		deletebtn = Button(btnFrame, text="Delete", width=10).grid(row=0, column=1, padx=10, pady=10)
-		updatebtn = Button(btnFrame, text="Update", width=10).grid(row=0, column=2, padx=10, pady=10)
-		clearbtn = Button(btnFrame, text="Clear", width=10).grid(row=0, column=3, padx=10, pady=10)
+		addbtn = Button(btnFrame, text="Add", width=10, font=("Consolas", 10, "bold")).grid(row=0,column=0, padx=10,pady=10)
+		deletebtn = Button(btnFrame, text="Delete", width=10, font=("Consolas", 10, "bold")).grid(row=0, column=1, padx=10, pady=10)
+		updatebtn = Button(btnFrame, text="Update", width=10, font=("Consolas", 10, "bold")).grid(row=0, column=2, padx=10, pady=10)
+		clearbtn = Button(btnFrame, text="Clear", width=10, font=("Consolas", 10, "bold")).grid(row=0, column=3, padx=10, pady=10)
 
 
 		# ============== Record Showing Frame ================
 				# This frame contain list box where  values will shows.
 		recordFrame = LabelFrame(self.root, text="Showing Records", font=("Time Roman", 20, "bold"), bd=4 ,fg='Black', bg="#ff9933")
+		recordFrame.place(x=500, y=70, width=830, height=600)
 
-		recordFrame.place(x=500, y=70, width=800, height=550)
+		searchLabel = Label(recordFrame, text="Search : ", font=("Consolas", 18, "bold"), fg="#adfc03", bg="#ff9933")
+		searchLabel.grid(row=0, column=0, padx=10, pady=10)
+
+		com_gen = ttk.Combobox(recordFrame, font=("Consolas", 18, "bold"),width=10, state="readonly")
+		com_gen["values"] = ("Male", "Female", "Other")
+		com_gen.grid(row=0, column=1, padx=5, pady=10)
+
+		txt_Search = Entry(recordFrame, font=("Consolas", 15, "bold"), bd=5, relief="ridge", width=25)
+		txt_Search.grid(row=0, column=2, padx=5, pady=10, sticky="w")
+
+		searchBtn = Button(recordFrame, text="Search ", width=8, font=("Consolas", 14, "bold")).grid(row=0, column=3, padx=10, pady=10)
+		showBtn = Button(recordFrame, text="ShowAll ", width=8, font=("Consolas", 14, "bold")).grid(row=0, column=4, padx=10, pady=10)
+
 
 if __name__ == "__main__":
 	win = Tk()
