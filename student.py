6ec2467fc616ab9688a104ad480be43a8ebc8f2f
kunal <53283003+kunal9922@users.noreply.github.com>
2020-11-13 21:35:18 +0530

    RecordFrame widget part Done. GUI complete

--- student.py
+++ student.py
@@ -39,21 +39,21 @@ class StudentRecord:
 		txt_Email = Entry(manageFrame, font=("Consolas", 15, "bold"), bd=2, relief="ridge", width=25)
 		txt_Email.grid(row=3, column=1, padx=10, pady=10, sticky="w")
 
-		lbl_address = Label(manageFrame, text="Address : ", font=("", 18, "bold"), fg="#adfc03", bg="#ff9933")
-		lbl_address.grid(row=4, column=0, padx=10, pady=10, sticky="w")
-		txt_address = Text(manageFrame, font=("Consolas", 15, "bold"), bd=2, relief="ridge", width=25, height=5)
-		txt_address.grid(row=4, column=1, padx=10, pady=10, sticky="w")
-
 		lbl_gender = Label(manageFrame, text="Gender : ", font=("Consolas", 18, "bold"), fg="#adfc03", bg="#ff9933")
-		lbl_gender.grid(row=5, column=0, padx=10, pady=10, sticky="w")
+		lbl_gender.grid(row=4, column=0, padx=10, pady=10, sticky="w")
 		com_gen = ttk.Combobox(manageFrame, font=("Consolas", 18, "bold"),state="readonly")
 		com_gen["values"] = ("Male", "Female", "Other")
-		com_gen.grid(row=5, column=1, padx=10, pady=10)
+		com_gen.grid(row=4, column=1, padx=10, pady=10)
 
 		lbl_dob = Label(manageFrame, text="D.O.B :", font=("", 18, "bold"), fg="#adfc03", bg="#ff9933")
-		lbl_dob.grid(row=6, column=0, padx=10, pady=10, sticky="w")
+		lbl_dob.grid(row=5, column=0, padx=10, pady=10, sticky="w")
 		txt_dob = Entry(manageFrame, font=("Consolas", 15, "bold"), bd=2, relief="ridge", width=25)
-		txt_dob.grid(row=6, column=1, padx=10, pady=10, sticky="w")
+		txt_dob.grid(row=5, column=1, padx=10, pady=10, sticky="w")
+
+		lbl_address = Label(manageFrame, text="Address : ", font=("", 18, "bold"), fg="#adfc03", bg="#ff9933")
+		lbl_address.grid(row=6, column=0, padx=10, pady=10, sticky="w")
+		txt_address = Text(manageFrame, font=("Consolas", 15, "bold"), bd=2, relief="ridge", width=25, height=5)
+		txt_address.grid(row=6, column=1, padx=10, pady=10, sticky="w")
 
 		#========== button frame ==========
 		btnFrame = LabelFrame(manageFrame, bd=6, relief ="solid", fg='Black', bg="#7bfc03")
@@ -73,8 +73,8 @@ class StudentRecord:
 		searchLabel = Label(recordFrame, text="Search : ", font=("Consolas", 18, "bold"), fg="#adfc03", bg="#ff9933")
 		searchLabel.grid(row=0, column=0, padx=10, pady=10)
 
-		com_gen = ttk.Combobox(recordFrame, font=("Consolas", 18, "bold"),width=10, state="readonly")
-		com_gen["values"] = ("Male", "Female", "Other")
+		com_gen = ttk.Combobox(recordFrame, font=("Consolas", 18, "bold"), width=10, state="readonly")
+		com_gen["values"] = ("Roll Num", "Name", "Semester", "Email", "Gender", "DateOfBirth", "Address")
 		com_gen.grid(row=0, column=1, padx=5, pady=10)
 
 		txt_Search = Entry(recordFrame, font=("Consolas", 15, "bold"), bd=5, relief="ridge", width=25)
@@ -83,6 +83,27 @@ class StudentRecord:
 		searchBtn = Button(recordFrame, text="Search ", width=8, font=("Consolas", 14, "bold")).grid(row=0, column=3, padx=10, pady=10)
 		showBtn = Button(recordFrame, text="ShowAll ", width=8, font=("Consolas", 14, "bold")).grid(row=0, column=4, padx=10, pady=10)
 
+		#=========== Table Data Frame ============
+		tableFrame = Frame(recordFrame, bd=2, relief="ridge", bg="Crimson" )
+		tableFrame.place(x=10, y=70, width=780, height=480)
+
+		# ========== showing stored record on this frame ==========
+		scroll_X = Scrollbar(tableFrame, orient=HORIZONTAL)
+		scroll_Y = Scrollbar(tableFrame, orient=VERTICAL)
+		studentRecordTable = ttk.Treeview(tableFrame, columns=("roll", "name", "sem", "email", "gender", "dob", "address"), xscrollcommand=scroll_X.set, yscrollcommand=scroll_Y.set)
+		scroll_X.pack(side=BOTTOM, fill=X)
+		scroll_Y.pack(side=RIGHT, fill=Y)
+		scroll_X.config(command=studentRecordTable.xview)
+		scroll_Y.config(command=studentRecordTable.yview)
+		studentRecordTable.heading("roll", text="Roll Num")
+		studentRecordTable.heading("name", text="Name")
+		studentRecordTable.heading("sem", text="Semester")
+		studentRecordTable.heading("email", text="Email")
+		studentRecordTable.heading("gender", text="Gender")
+		studentRecordTable.heading("dob", text="DateOFBirth")
+		studentRecordTable.heading("address", text="Address")
+		studentRecordTable["show"] = "headings"
+		studentRecordTable.pack(expand=True,fill=BOTH)
 
 if __name__ == "__main__":
 	win = Tk()
