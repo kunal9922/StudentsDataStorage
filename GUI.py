810cd225c4f4b49c58443a595e329ce9285ccfe7
kunal <53283003+kunal9922@users.noreply.github.com>
2020-11-26 22:37:22 +0530

    create listBox where all available DataBases will show

--- GUI.py
+++ GUI.py
@@ -2,9 +2,25 @@ import tkinter as tk
 from tkinter import messagebox
 class ShowDBFrame():
 	def DBFrame(self, rootWin):
-		pass
+		self.lableDBframe = tk.LabelFrame(rootWin, text="DataBase Entry", font=("Time Roman", 20, "bold"), bd=4,width=400, height=200)
+		'''dblabel = tk.Label(self.lableDBframe, text="Have DATABASE ?")
+		dbHave = tk.Checkbutton(rootWin, text="Yes", onvalue=1, offvalue=0)
+		dbHaveNot = tk.Checkbutton(rootWin, text="No", onvalue=1, offvalue=0)
+		'''
+		# place where all available DB will show
+		self.lableDBframe.grid(row=3, column=0, padx=10, pady=10)
+		xscroll = tk.Scrollbar(self.lableDBframe,orient=tk.HORIZONTAL)
+		yscroll = tk.Scrollbar(self.lableDBframe, orient=tk.VERTICAL)
+		listDBs= tk.Listbox(self.lableDBframe, bg="#22e6d5", fg="#000000", bd=2, relief="ridge", font=("Time Roman", 20, "bold"), xscrollcommand=xscroll.set, yscrollcommand=yscroll.set)
 
-class GUI_project():
+		xscroll.pack(fill=tk.X, side=tk.BOTTOM)
+		xscroll.config(command=listDBs.xview)
+		yscroll.config(command=listDBs.yview)
+		yscroll.pack(fill=tk.Y, side=tk.RIGHT)
+
+		listDBs.pack()
+
+class GUI_project(ShowDBFrame):
 	def __init__(self, RootWin):
 		# toplevel window for connectivity to database
 		self.RootWin = RootWin
@@ -35,5 +51,7 @@ class GUI_project():
 
 		#	hostName = tk.Label(topWin, text=" DB Name : ", font=("", 18, "bold"), fg="#adfc03", bg="#ff9933")
 		#hostName.grid(row=3, column=0, padx=20, pady=10)
+
+		ShowDBFrame.DBFrame(self, rootWin=topWin)
 
 
