4ef23778a343b5413ee76b9c957edbefc3657990
Kunal Soni <kunalsoni9200@gmail.com>
2023-08-08 17:25:17 +0530

    Fix: #1 Change the CURD operation for normalization in SQL

--- DBconnect.py
+++ DBconnect.py
@@ -19,10 +19,19 @@ class DB_connect():
 
 
 	def addIntoDB(self, items: tuple):
-		'''Rollnum, Name, contact, Email, Gender, DOB, Address'''
+		'''gender, contact, dob, address, rollnum, first_name, last_name, email'''
+		# items = ('Male', '1234567890', '2000-01-01', '123 Main St', 101, 'John', 'Doe', 'john@example.com')
 		#query to insert into DB table
-		sqlQuery = f"INSERT INTO {self.table} values(%s, %s, %s, %s, %s, %s, %s )"
-
+		sqlQuery = f"BEGIN; -- Start the transaction\
+			-- Insert into Basic table\
+			INSERT INTO {self.table}Basic (gender, contact, dob, address)\
+			VALUES (%s, %s, %s, %s);\
+			-- Get the last inserted Basic id\
+			SET @basic_id = LAST_INSERT_ID();\
+			-- Insert into Record table\
+			INSERT INTO {self.table} (rollnum, first_name, last_name, email, basic_id)\
+			VALUES (%s, %s, %s, %s, @basic_id);\
+			COMMIT; -- Commit the transaction"
 		try:
 			self.cursor.execute(sqlQuery, items)
 			self.mydb.commit()
@@ -33,20 +42,42 @@ class DB_connect():
 			return False
 
 	def gettingData(self):
-		sqlQuery = f"SELECT * FROM {self.table}"  # getting all attribute from Table
+		sqlQuery = f"SELECT r.rollnum, r.first_name, r.last_name, r.email, b.gender, b.contact, b.dob, b.address\
+				FROM {self.table} r\
+				JOIN {self.table}Basic b ON r.basic_id = b.id;"
+		# getting all attribute from Table
 		print(sqlQuery)
 		self.cursor.execute(sqlQuery)
-		rows = self.cursor.fetchall()
-		return rows
+		return self.cursor.fetchall()
+		
 
 	def updateData(self, items: tuple):
-		sqlQuery = f"UPDATE {self.table} SET RollNum=%s, Name=%s, Contact=%s, Email=%s, Gender=%s, DOB=%s, Adress=%s WHERE RollNum=%s"
+		sqlQuery = f"BEGIN; -- Start the transaction\
+			UPDATE {self.table}Basic as b\
+			JOIN {self.table} AS r ON b.id = r.basic_id\
+			SET b.gender = %s,\
+				b.contact = %s,\
+				b.dob = %s, -- New value for dob\
+				b.address = %s\
+			WHERE r.rollnum = %s; -- Use the appropriate condition to identify the record in the Basic table\
+			UPDATE {self.table}\
+			SET first_name = %s,\
+				last_name = %s,\
+				email = %s,\
+			WHERE rollnum = %s; -- Use the appropriate condition to identify the record in the Record table\
+			COMMIT; -- Commit the transaction"
 		self.cursor.execute(sqlQuery, items)
 		self.mydb.commit()
 
 	def deleteData(self, key: int):
 
-		sqlQuery = f"DELETE FROM {self.table} WHERE RollNum={key}"
+		sqlQuery = f"BEGIN; -- Start the transaction\
+			-- Delete from Record table\
+			DELETE FROM {self.table} WHERE rollnum = %s; -- Use the appropriate condition to identify the record in the Record table\
+			-- Delete associated Basic data\
+			DELETE FROM {self.table}Basic WHERE id NOT IN (SELECT basic_id FROM {self.table});\
+			COMMIT; -- Commit the transaction\
+			"
 		print(sqlQuery)
 		self.cursor.execute(sqlQuery)
 		self.mydb.commit()
@@ -78,7 +109,28 @@ class DB_connect():
 		self.mydb.commit()
 
 	def createtable(self, tableName):
-		sqlQuery = f"CREATE TABLE IF NOT EXISTS {tableName}( RollNum INT(12) PRIMARY KEY, Name varchar(30) NOT NULL, Contact varchar(10), Email varchar(40), Gender varchar(6), DOB varchar(10), Adress varchar(80))"
+		"""
+		Creates two connected tables in the database: {tableName}Basic and {tableName}.
+		{tableName}Basic stores ID, gender, contact, DOB, and address.
+		{tableName} stores roll number, names, email, and links to {tableName}Basic.
+		"""
+		self.table = tableName
+		sqlQuery = f'CREATE TABLE IF NOT EXISTS {self.table}Basic (\
+					id INT PRIMARY KEY,\
+					gender VARCHAR(10),\
+					contact VARCHAR(15),\
+					dob DATE,\
+					address VARCHAR(30)\
+				);'
+		self.cursor.execute(sqlQuery)
+		sqlQuery = f'CREATE TABLE IF NOT EXISTS {self.table}(\
+					rollnum INT PRIMARY KEY,\
+					first_name VARCHAR(20),\
+					last_name VARCHAR(20),\
+					email VARCHAR(30),\
+					basic_id INT,\
+					FOREIGN KEY (basic_id) REFERENCES {self.table}Basic(id)\
+				);'
 		self.cursor.execute(sqlQuery)
 		self.mydb.commit()
 	
